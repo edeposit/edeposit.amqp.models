9ea2d595247f61363394c0f6cc6546f167c71dcd
Jan Stavěl <stavel.jan@gmail.com>
2015-10-15 16:14:49 +0200

    doplnil jsem schemata

--- src/edeposit/amqp/models/content.py
+++ src/edeposit/amqp/models/content.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 from schema import Schema, And, Use, Optional
 
+producent = Schema({
+    'title': And(unicode,len),
+    'id':And(str,len),
+    Optional('sysnumber'): And(str,len)
+})
+
+declarer = Schema({
+    'username':And(str,len)
+})
 
 epublication = Schema({
     'title': And(unicode,len),
     Optional('podnazev'): And(unicode,len),
-    
+    Optional('cast'): And(unicode,len),
+    Optional('nazev_casti'): And(unicode,len),
+    Optional('isbn_souboru_publikaci'): And(unicode,len),
+    'poradi_vydani': And(unicode,len),
+    'misto_vydani': And(unicode,len),
+    'rok_vydani': And(unicode,len),
+    'nakladatel_vydavatel': producent.validate,
+    'zpracovatel_zaznamu': declarer.validate
 })
