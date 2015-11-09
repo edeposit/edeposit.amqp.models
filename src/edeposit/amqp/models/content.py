dfee98f1fc2046b5328c1db387435ff941e94e39
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-09 17:11:34 +0100

    Fixed few bugs, reformated.

--- src/edeposit/amqp/models/content.py
+++ src/edeposit/amqp/models/content.py
@@ -1,44 +1,44 @@
-#!/usr/bin/python
-from schema import Schema, And, Use, Optional
-from isbn_validator import is_valid_isbn
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
 from functools import partial
 from operator import contains
-import .libraries
-import .riv
 
-producent = Schema({
-    'title': And(unicode,len),
-    'id':And(str,len),
-    Optional('sysnumber'): And(str,len)
-})
+from schema import Schema
+from schema import And
+from schema import Decimal
+from schema import Optional
 
-declarer = Schema({
-    'username':And(str,len)
-})
+from isbn_validator import is_valid_isbn
 
+from . import riv
+from . import libraries
+
+
+# Validators ==================================================================
 epublication = Schema({
-    'title': And(unicode,len),
-    Optional('podnazev'): And(unicode,len),
-    Optional('cast'): And(unicode,len),
-    Optional('nazev_casti'): And(unicode,len),
-    Optional('isbn'): is_valid_isbn,
-    Optional('generated_isbn'): partial(isinstance,bool),
-    Optional('isbn_souboru_publikaci'): is_valid_isbn,
-    Optional('author1'): And(unicode,len),
-    Optional('author2'): And(unicode,len),
-    Optional('author3'): And(unicode,len),
-    'poradi_vydani': And(unicode,len),
-    'misto_vydani': And(unicode,len),
+    'title': And(unicode, len),
+    'poradi_vydani': And(unicode, len),
+    'misto_vydani': And(unicode, len),
     'rok_vydani': int,
-    'misto_vydani': And(unicode,len),
-    'nakladatel_vydavatel': producent.validate,
-    Optional('vydano_v_koedici_s'): And(unicode,len),
-    Optional('cena'):Decimal,
-    libraries_accesing: partial(isinstance,bool),
-    libraries_that_can_access: partial(contains,libraries.ids),
-    Optional(offer_to_riv): partial(isinstance,bool),
-    Optional(category_for_riv):And(int,partial(contains,riv.ids)),
-    'zpracovatel_zaznamu': declarer.validate,
-    Optional(url): And(str,len),
-    Optional(anotace): And(unicode,len)
+    'misto_vydani': And(unicode, len),
+    "libraries_that_can_access": partial(contains, libraries.ids),
+    'zpracovatel_zaznamu': "",
+    Optional('podnazev'): And(unicode, len),
+    Optional('cast'): And(unicode, len),
+    Optional('nazev_casti'): And(unicode, len),
+    Optional('isbn'): is_valid_isbn,
+    Optional('isbn_souboru_publikaci'): is_valid_isbn,
+    Optional('author1'): And(unicode, len),
+    Optional('author2'): And(unicode, len),
+    Optional('author3'): And(unicode, len),
+    Optional('nakladatel_vydavatel'): And(unicode, len),
+    Optional('vydano_v_koedici_s'): And(unicode, len),
+    Optional('cena'): Decimal,
+    Optional("category_for_riv"): And(int, partial(contains, riv.ids)),
+    Optional("url"): And(str, len),
+    Optional("anotace"): And(unicode, len)
 })
