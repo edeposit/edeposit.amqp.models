e6223bc675bb9ed7930b592d1f1be638f8232885
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-13 14:21:34 +0100

    SchemaError moved to module level.

--- src/edeposit/amqp/models/__init__.py
+++ src/edeposit/amqp/models/__init__.py
@@ -4,6 +4,7 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+from epublication import SchemaError
 from epublication import EpublicationValidator
 from epublication import czech_to_edeposit_dict
 from epublication import edeposit_to_czech_dict
